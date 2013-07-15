7b7f207bd2868ea2db340458b10a13630f9c03b7
Nicolas Stadler <n.staedler@yahoo.de>
2013-07-15 18:26:14 +0200

    auxfunc.r: new function sparse_conc_v2; this function does not fix cond(Omega)=p; this function allows generating GGMs with various SNR (=parcor)

--- diffnet/auxfunc.r
+++ diffnet/auxfunc.r
@@ -48,6 +48,57 @@ sparse_conc <- function(p,K,s,s.common,magn.nz=0.5,scale.parcor=TRUE){
     return(SigInv)
 }
 
+sparse_conc_v2<- function(p,K,s,s.common,magn.nz=0.5,scale.parcor=TRUE){
+    ##Generate K different Sparse Inverse Covariance-Matrices of dimension p:
+    ##
+    ##-for each SigInv there are s non-zero entries
+    ##-s.common locations of non-zero entries are common among all SigInv;
+    ## whereas s-s.common non-zero entries are at different locations
+
+  if(s==0){
+    SigInv <- list()
+    for (k in 1:K){
+      SigInv[[k]] <- diag(1,p)
+    }
+  }else{
+    
+
+    ind.upper.tri <- which(upper.tri(matrix(NA,p,p)))
+    
+    B.list <- list()
+    B <- matrix(0,p,p)
+    comp.nonzero <- tot.nonzero <- sample(ind.upper.tri,size=s,replace=FALSE)
+    same.nonzero <- sample(comp.nonzero,size=s.common,replace=FALSE)
+    remain.zero <- setdiff(ind.upper.tri,comp.nonzero)
+    B[comp.nonzero] <- magn.nz
+    B.list[[1]] <- B+t(B)
+    if (K>1){
+      for (k in 2:K){
+        B <- matrix(0,p,p)
+        comp.nonzero <- c(same.nonzero,sample(remain.zero,size=s-s.common,replace=FALSE))
+        tot.nonzero <- union(tot.nonzero,comp.nonzero)
+        B[comp.nonzero] <- magn.nz
+        B.list[[k]] <- B+t(B)
+        remain.zero <- setdiff(ind.upper.tri,tot.nonzero)
+      }
+    }
+
+    SigInv <- list()
+    for (k in 1:K){
+      SigInv[[k]] <- B.list[[k]]
+      ev <- eigen(SigInv[[k]])$values
+      del <- abs(min(ev))+0.01
+      diag(SigInv[[k]]) <- del
+      if(scale.parcor==TRUE){
+        SigInv[[k]] <- SigInv[[k]]/del
+      }
+    }
+  }
+    
+    return(SigInv)
+}
+
+
 ##' High-Dim Two-Sample Test based on multiple correction
 ##'
 ##' perform univariate t-tests and correct for multiple comparison
