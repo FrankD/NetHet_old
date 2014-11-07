e2675177f930cbf26d734bf1519d1966e8a2cd49
FrankD <fdondelinger.work@gmail.com>
2014-11-07 14:51:53 +0000

    Changed requested by Nate Hayden: Length of lines in betamat calculation and extensions of R files.

--- nethet/src/betamat_diffnet.c
+++ nethet/src/betamat_diffnet.c
@@ -15,7 +15,12 @@ void betamat_diffnet(double *betamat, int *ind1, int *ind2, int *uptrirownr,
     for (j=0;j<l2;j++){
       colnrj=uptricolnr[ind2[j]]-1;
       rownrj=uptrirownr[ind2[j]]-1;
-      betamat[l1*j+i]=sig[colnri+kk*rownri]*sig[colnrj+kk*rownrj]-sig1[colnri+kk*rownri]*sig[colnrj+kk*rownrj]-sig[colnri+kk*rownri]*sig2[colnrj+kk*rownrj]+sig1[colnri+kk*rownri]*sig2[colnrj+kk*rownrj]+sig[colnri+kk*rownrj]*sig[rownri+kk*colnrj]+sig[colnri+kk*colnrj]*sig[rownri+kk*rownrj];
+      betamat[l1*j+i] = sig[colnri+kk*rownri]*sig[colnrj+kk*rownrj] -
+        sig1[colnri+kk*rownri]*sig[colnrj+kk*rownrj] - 
+        sig[colnri+kk*rownri]*sig2[colnrj+kk*rownrj] + 
+        sig1[colnri+kk*rownri]*sig2[colnrj+kk*rownrj] + 
+        sig[colnri+kk*rownrj]*sig[rownri+kk*colnrj] + 
+        sig[colnri+kk*colnrj]*sig[rownri+kk*rownrj];
    
     }
   }
